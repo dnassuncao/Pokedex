d2ace3840bcb461effc0d9b0a4d974624d040f62
Diêgo Nunes <diegonunes.sistema@gmail.com>
2024-03-23 15:39:40 -0300

    create pokemon list item

--- app/src/main/java/br/com/dnassuncao/pokedex/core/PokemonCategory.kt
+++ app/src/main/java/br/com/dnassuncao/pokedex/core/PokemonCategory.kt
@@ -3,25 +3,32 @@ package br.com.dnassuncao.pokedex.core
 import androidx.compose.ui.graphics.Color
 import br.com.dnassuncao.pokedex.R
 import br.com.dnassuncao.pokedex.ui.theme.ColorFire
+import br.com.dnassuncao.pokedex.ui.theme.ColorFireBackground
 import br.com.dnassuncao.pokedex.ui.theme.ColorGrass
+import br.com.dnassuncao.pokedex.ui.theme.ColorGrassBackground
 import br.com.dnassuncao.pokedex.ui.theme.ColorWater
+import br.com.dnassuncao.pokedex.ui.theme.ColorWaterBackground
 
 sealed class PokemonCategory(
-    val icon : Int,
-    val color : Color
+    val icon: Int,
+    val color: Color,
+    val backgroundColor: Color,
 ) {
     data object Water : PokemonCategory(
         icon = R.drawable.ic_category_water,
-        color = ColorWater
+        color = ColorWater,
+        backgroundColor = ColorWaterBackground
     )
 
     data object Grass : PokemonCategory(
         icon = R.drawable.ic_category_grass,
-        color = ColorGrass
+        color = ColorGrass,
+        backgroundColor = ColorGrassBackground
     )
 
     data object Fire : PokemonCategory(
         icon = R.drawable.ic_category_fire,
-        color = ColorFire
+        color = ColorFire,
+        backgroundColor = ColorFireBackground
     )
 }
--- app/src/main/java/br/com/dnassuncao/pokedex/features/search/presentation/components/PokeImageView.kt
+++ app/src/main/java/br/com/dnassuncao/pokedex/features/search/presentation/components/PokeImageView.kt
@@ -2,25 +2,25 @@ package br.com.dnassuncao.pokedex.features.search.presentation.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.dnassuncao.pokedex.R
 import br.com.dnassuncao.pokedex.core.PokemonCategory
-import br.com.dnassuncao.pokedex.ui.theme.Typography
 
 @Composable
 fun PokeImageView(
@@ -28,26 +28,34 @@ fun PokeImageView(
     category: PokemonCategory,
     modifier: Modifier = Modifier
 ) {
-    Card(
-        modifier = modifier,
-        shape = RoundedCornerShape(12.dp)
+    Row(
+        modifier = modifier
+            .clip(shape = RoundedCornerShape(
+                topStart = 12.dp,
+                bottomStart = 12.dp
+            ))
+            .background(category.color)
+        ,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+
     ) {
         Box(
             modifier = Modifier
+                .wrapContentWidth()
                 .padding(4.dp)
         ) {
             Image(
                 painter = painterResource(id = category.icon),
                 modifier = Modifier
-                    .size(50.dp)
+                    .size(80.dp)
                     .align(Alignment.Center),
-                alpha = 0.2F,
                 contentDescription = ""
             )
             Image(
                 painter = painterResource(id = image),
                 modifier = Modifier
-                    .size(25.dp)
+                    .size(35.dp)
                     .align(Alignment.Center),
                 contentDescription = ""
             )
